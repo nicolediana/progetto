01d32fa9b5b26e36cc79e955ed5c509223663030
nicolediana <einsiedeln_ch@libero.it>
2015-05-08 23:35:39 +0200

    Inserito tiporichiesta "prossima partita" in ServletPartita

--- ServletExample/src/ServletPartita.java
+++ ServletExample/src/ServletPartita.java
@@ -355,58 +355,26 @@ public class ServletPartita extends HttpServlet {
 				int idprofilo = Integer.parseInt(jObj.get("idprofilo").toString());
 				ResultSet result;
 				int idPartita = 0;
-				boolean esito = false;				
+				boolean esito = false;	
+				int id = 0;
 		    	
 		    	//String sql = "SELECT idpartita FROM profilo_partita WHERE idprofilo='" + idprofilo + "'";
-		    	String sql = "SELECT * FROM partita " +
+		    	String sql = "SELECT partita.idpartita FROM partita " +
 		    				 "INNER JOIN profilo_partita ON partita.idpartita = profilo_partita.idpartita " +
 		    				 "WHERE partita.data > (SELECT NOW()) AND profilo_partita.idprofilo = " + idprofilo +
 		    				 " ORDER BY partita.data ASC LIMIT 1;";
 		    	result = stmt.executeQuery(sql);		    	
-		    	Partita partita = new Partita();
 		    	if(result.next()) {
 		    		esito = true;
-		    		partita.setIdpartita(Integer.parseInt(result.getString("idpartita")));	
-		    		partita.setIdtipopartita(Integer.parseInt(result.getString("idtipopartita")));					
-		    		partita.setNomecampo(result.getString("nomecampo"));
-		    		partita.setIndirizzocampo(result.getString("indirizzocampo"));
-		    		partita.setCosto(Float.parseFloat(result.getString("costo")));
-		    		partita.setData(result.getString("data"));
-		    		partita.setOra(result.getString("ora"));
-		    		partita.setCitta(result.getString("citta"));
-		    		partita.setProvincia(result.getString("provincia"));
-		    		partita.setLinkfotocampo(result.getString("linkfotocampo"));
-		    		partita.setTerreno(result.getString("terreno"));
-		    		partita.setCoperto(result.getString("coperto"));
-		    		partita.setNote(result.getString("note"));
-					partita.setAmministratore(Integer.parseInt(result.getString("amministratore")));
-					partita.setContatto(result.getString("contatto"));					
+		    		id = Integer.parseInt(result.getString("partita.idpartita"));			    						
 				}
 		    	else esito = false;
 		    	
 		    	JSONObject jsonObj = new JSONObject();
-				jsonObj.put("prossimaPartita", partita);
+				jsonObj.put("idPartita", id);
 				jsonObj.put("esito", esito);
 				writer.write(jsonObj.toString());
-		    }
-			
-			/*
-			if(tiporichiesta.equals("caricaTipoPartita"))
-			{
-				Vector<String> v = new Vector<String>();
-				
-			    String sql="";
-			    sql = "SELECT * FROM tipopartita";			    
-				rs = stmt.executeQuery(sql);
-				while(rs.next()) {
-			    	v.add(rs.getString("tipopartita"));
-				}
-				
-				JSONObject jsonObj = new JSONObject();
-				jsonObj.put("jsonVector", v);
-				writer.write(jsonObj.toString());
-			}*/
-		
+		    }					
 		} catch (SQLException | JSONException e) {
 			  e.printStackTrace();
 	  }
